c63ccaef975cff59e32f9b5bce50314bd857f03e
Rick Cobb <rick@grandrounds.com>
2019-03-12 16:30:58 -0700

    Git rebase is *always* evil. Restore missing test.

--- test/support/valid_task_too_long_description/script/schleppers/20160906160611_my_task.rb
+++ test/support/valid_task_too_long_description/script/schleppers/20160906160611_my_task.rb
@@ -1,13 +1,11 @@
 require 'active_support/core_ext/string'
 
-class MyTask < Schlepper::Task
+class LongUTF8Description < Schlepper::Task
   attr_reader :failure_message
 
   # @return [String] A short note on what the purpose of this task is
   def description
-     <<-DOC.strip_heredoc
-       No doc
-     DOC
+    'wackamöle \u1F60 ' * 26
   end
 
   # @return [String] The individuals that owns this task
